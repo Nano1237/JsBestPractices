02479e8b654e37e93d68792b17ad891318c000ad
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2013-06-14 09:26:19 +0200

    manually merged some changes from HTML5Security back to JSBestPractices

---
+++ plugin/showSource/showSource.js
@@ -0,0 +1,63 @@
+﻿(function ($) {
+
+    var startSnip = "<!--START-->";
+    var stopSnip = "<!--STOP-->";
+
+    var extractSnippet = function(data) {
+        var match = data.match(startSnip + "([^]*?)" + stopSnip);
+                  
+        if (match !== null) {
+            return match[1].trim();
+        }
+
+        // fallback, if no snippet was placed
+        return data;
+    };
+
+    var httpRequest = function(url) {
+        return $.ajax({
+            url: url,
+            dataType: "text"
+        });
+    };
+
+    var replaceCode = function() {
+        $(document.body).find('code[data-show-source]').each(function () {
+
+            var code = this;
+            var url = $(this).data('show-source');
+            
+            httpRequest(url).done(function (data) {
+
+                var snippet = extractSnippet(data);
+                var snippetEncoded = $('<div />').text(snippet).html();
+                code.innerHTML = snippetEncoded;
+                
+                // re-render syntax highlighting
+                if (typeof window.hljs == "object") {
+                    window.hljs.highlightBlock(code);
+                }
+            });
+        });
+    };
+    
+    var insertHtml = function () {
+        $(document.body).find('div[data-insert-source]').each(function () {
+
+            var code = this;
+            var url = $(this).data('insert-source');
+
+            httpRequest(url).done(function (data) {
+
+                var snippet = extractSnippet(data).trim();
+                code.innerHTML = snippet;
+            });
+        });
+    };
+
+    $(function() {
+        replaceCode();
+        insertHtml();
+    });
+
+})(window.jQuery);
